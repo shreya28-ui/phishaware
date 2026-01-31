966db5f74b3af056169c0f22c8fc1e876759f03c
shreya28-ui <shreyashreemurthy89@gmail.com>
2026-01-31 23:07:41 +0530

    Update Firebase config for deployment and add .env.example

--- firebase/config.ts
+++ firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-2373398491-d9308",
-  "appId": "1:804964051143:web:0e7e53120cc53d79daf6e2",
-  "apiKey": "AIzaSyDQNOGyCRdXWYMOW3DW_4bkMdUKtZgWYxc",
-  "authDomain": "studio-2373398491-d9308.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "804964051143"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY || "AIzaSyDQNOGyCRdXWYMOW3DW_4bkMdUKtZgWYxc",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN || "studio-2373398491-d9308.firebaseapp.com",
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID || "studio-2373398491-d9308",
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID || "804964051143",
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID || "1:804964051143:web:0e7e53120cc53d79daf6e2",
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID || ""
 };
